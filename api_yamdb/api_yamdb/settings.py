3822c0651f777501f2c97bec762dda47b8bf434d
Евгения Воропай <evoropay@gmail.com>
2023-02-21 02:27:03 +0300

    aaa

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,8 @@ load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='string')
+# SECRET_KEY = os.getenv('SECRET_KEY', default='string')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 if os.getenv('ENVIRONMENT') == 'dev':
     DEBUG = True
